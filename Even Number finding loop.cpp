569598fb86a1f0d96326f21a886c3c34a0d3fd7b
prabhat946 <chakravarthula.prabhat.btech2023@sitpune.edu.in>
2024-08-07 15:36:05 +0530

    Add files via upload

---
+++ Even Number finding loop.cpp
@@ -0,0 +1,25 @@
+//Name:prabhat
+//PRN:23070123040
+//Aim:Implemention of Decision Making Statements Loops to finding Even number between 1 and 10
+#include <iostream>
+using namespace std;
+
+int main() {
+ for(int i=0;i<11;i++) {
+    if(i%2==0)
+    {cout << i<< endl;}
+    
+    }
+
+    return 0;
+}
+
+
+/*Output:
+0
+2
+4
+6
+8
+10
+*/
