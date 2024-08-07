6192f8de4a033e603d18c7e18752f86394eb479f
prabhat946 <chakravarthula.prabhat.btech2023@sitpune.edu.in>
2024-08-07 15:37:05 +0530

    Add files via upload

---
+++ Triangle Pattern.cpp
@@ -0,0 +1,35 @@
+//Name:prabhat
+//PRN:23070123040
+//Aim:Implemention of Decision Making Statements Loops to print Triangle Pattern
+
+#include <iostream>
+using namespace std;
+
+int main(){
+   
+    int n;
+
+    cout<<"Enter The Value for n: ";
+    cin>>n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+        for (int k = 1; k <= i; k++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+/*Output:
+Enter The Value for n: 5
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+*/
