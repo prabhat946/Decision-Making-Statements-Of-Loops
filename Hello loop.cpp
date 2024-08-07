7268c3f4005c597b75f3f3e501d06aebf6b902d7
prabhat946 <chakravarthula.prabhat.btech2023@sitpune.edu.in>
2024-08-07 15:35:11 +0530

    Add files via upload

---
+++ Hello loop.cpp
@@ -0,0 +1,25 @@
+//Name:prabhat
+//PRN:23070123040
+//Aim:Implemention of Decision Making Statements Loops to print Hello Five times 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() { 
+    string a;
+    a = "Hello";
+for(int i=0;i<5;i++)
+
+{cout << "Hello"<< endl;}
+
+    return 0;
+}
+
+
+/*Output:
+Hello
+Hello
+Hello
+Hello
+Hello*/
