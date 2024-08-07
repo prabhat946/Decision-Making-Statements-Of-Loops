9333e71f0fcbcccbe8bfdc1fa9871c4a97547c0f
prabhat946 <chakravarthula.prabhat.btech2023@sitpune.edu.in>
2024-08-07 15:35:24 +0530

    Add files via upload

---
+++ Inverted Simple Pyramid.cpp
@@ -0,0 +1,31 @@
+//Name:prabhatS
+//PRN:23070123040
+//Aim:Implemention of Decision Making Statements Loops to print Inverted Simple Pyramid
+
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int n;
+ 
+    cout<<"Enter The Value for n: ";
+    cin>>n;
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+ 
+    return 0;
+}
+
+
+/*Output:
+Enter The Value for n: 5
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+*/
